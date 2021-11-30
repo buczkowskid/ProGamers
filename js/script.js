c8201acdbeb10851adcf1e8b47f4e3c4c195a782
voodoo96 <dominik.buczkowski96@gmail.com>
2021-11-30 21:08:25 +0100

    new scripts, added about us section

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// DomContentLoaded - dopiero po wczytaniu całej strony uruchomia się funkcje JS
+document.addEventListener("DOMContentLoaded", function () {
+	const nav = document.querySelector(".navbar");
+	const navShow = document.querySelector(".navbar-collapse");
+	const navItems = document.querySelector(".navbar-nav");
+
+	//Adding shadow on the navigation
+	function addShadow() {
+		//IF, który sprawdzi, czy window.scrolly jest większy lub równy    200,  (true) dodaj do zmiennej nav klase shadow-bg (false) usuń .shadow-bg
+		if (window.scrollY >= 200) {
+			nav.classList.add("shadow-bg");
+		} else {
+			nav.classList.remove("shadow-bg");
+		}
+	}
+
+	//Hiding navigation after click
+	function hideNav() {
+		if (navShow.classList.contains("show")) {
+			navShow.classList.remove("show");
+		}
+	}
+
+	//Same function without IF
+	// navItems.forEach((item) =>
+	// 	item.addEventListener("click", () => navShow.classList.remove("show"))
+	// );
+
+	//after scrolling start function
+	window.addEventListener("scroll", addShadow);
+
+	//after click any nav 'a' navigation is hiding
+	navItems.addEventListener("click", hideNav);
+});
